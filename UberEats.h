b2ca0fc11aabcf5bb9b1ebf8c484120549af7689
bluwaffe <48776415+bluwaffe@users.noreply.github.com>
2019-04-01 11:32:35 -0300

    Novo Método
    criação de método "TempoPreparo" para receber o parâmetro "tp"(tempo) de acordo o tipo de comida solicitada pelo usuário e calcular o tempo de entrega

--- UberEats.h
+++ UberEats.h
@@ -1,7 +1,9 @@
 #ifndef UBEREATS_H
 #define UBEREATS_H
-
+#include <iostream>
 #include <string>
+
+using namespace std;
 using str::string;
 
 class UberEats
@@ -11,9 +13,20 @@ public:
 	~UberEats();
 
 private:
-	int NumPedido;
+	int TempoEntrega = 0;
 	string TipoComida;
-
+	void TempoPreparo (int tp);
 };
+
+	void UberEats::TempoPreparo(int tp)(
+	
+	)
+
+
+int main (){
+			
+	
+return 0;	
+}
 
 #endif // UBEREATS_H

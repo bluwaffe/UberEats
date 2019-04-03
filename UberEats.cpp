1008e955c49837415d36b1dcda12ee820bdd155e
bluwaffe <48776415+bluwaffe@users.noreply.github.com>
2019-04-03 13:58:24 -0300

    Update UberEats.cpp

--- UberEats.cpp
+++ UberEats.cpp
@@ -1,10 +1,19 @@
 #include "UberEats.h"
+#include <iostream>
+using std::cout;
+using std::endl;
 
-UberEats::UberEats()
+UberEats::UberEats(int primeirosPedidos)
 {
+  definePrimeirosPedidos(primeirosPedidos);
 }
 
 UberEats::~UberEats()
 {
 }
+void Ubereats::definePrimerosPedidos(int primeirosPedidos){
+    quantidadePedidos = primeirosPedidos;
+    std::cout<<"O numero de pedidos iniciais e/sao:"<<quantidadePedidos<<std::endl;
+}
 
+void UberEats::novosPedidos

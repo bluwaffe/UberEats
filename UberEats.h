41bb38a7978e8643191734ee51939edd1770e302
bluwaffe <48776415+bluwaffe@users.noreply.github.com>
2019-04-08 09:25:26 -0300

    Update UberEats.h

--- UberEats.h
+++ UberEats.h
@@ -8,12 +8,13 @@ class UberEats
 {
 public:
 	UberEats(int);//constructor1
-    //UberEats();//contructor2
+    	UberEats();//contructor2_Sobrecarregado
 	~UberEats( );
     
 	void definePrimeirosPedidos( int );
-    
-    void novosPedidos(int);
+	void novosPedidos(int);
+    	void novosPedidos();//sobrecarga_de_metodo
+
 	
 private:
 	int quantidadePedidos;

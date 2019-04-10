731223a7db7ba6e4d9a356936be3c5456cb01fcd
bluwaffe <48776415+bluwaffe@users.noreply.github.com>
2019-04-10 14:26:46 -0300

    Update trabalho
    Update do trabalho

--- UberEats.cpp
+++ UberEats.cpp
@@ -7,10 +7,11 @@ using std cin;
 using std::string;
 
 
-UberEats::UberEats(int primeirosPedidos, int entregadoresDisponiveis, string tipoPedido )
+UberEats::UberEats(int primeirosPedidos, int entregadores, string &tipo,  )
 {
-  definePrimeirosPedidos(primeirosPedidos);
-  
+	definePrimeirosPedidos(primeirosPedidos);
+	defineEntregadoresDisponiveis(entregadores);
+	defineTipoPedido(tipo);
 }
 
 UberEats::UberEats()
@@ -18,12 +19,43 @@ UberEats::UberEats()
   definePrimeirosPedidos(1);
 }
 
-UberEats::UberEats(const UberEats &p)
+UberEats::UberEats(const UberEats &p)//copia_construct
 {
     quantidaPedidos = p.quantidaPedidos;
     quantidadeEntregadores = p.quantidadeEntregadores;
     tipoPedido = p.tipoPedido;
-	entregadoresDisponiveis = p.entregadoresDisponiveis;
+entregadoresDisponiveis = p.entregadoresDisponiveis;
+	
+	
+UberEats::defineTipoPedido(string &tipo)
+{
+		
+	tipoPedido = tipo;
+	
+	
+	printTipoPedido();
+}
+
+UberEats::defineEntregadoresDisponiveis()
+{
+	entregadoresDisponiveis = entregadores;
+	
+	printEntregadoresDisponiveis();
+}
+
+UberEats::message() const
+{
+	cout<<"Os dados Foram carregados"<<endl;
+}
+
+UberEats::printEntregadoresDisponiveis() const
+{
+	cout<<"A quantidade de entregadores disponiveis é de : " entregadoresDisponiveis<<endl;
+}
+
+UberEats::printTipopedido() const
+{
+cout<<" O tipo do pedido é: " tipoPedido <<endl;
 }
 
 UberEats::~UberEats()
@@ -44,9 +76,7 @@ void UberEats::novosPedidos(int pedidosRecentes)
   quantidaPedidos += pedidosRecentes;
   std::cout<<"A quantidade de pedidos atualizada é de: "<<quantidaPedidos<<std::endl;
 }
-/*void menssage(const string &msg) const{
-    cout
-}*/
+
 
 void UberEats::novosPedidos()
 {
